5b99cc26ba97d64d89a8cfa2f875a593442bcfd0
sunshane6726 <sunshane6726@gmail.com>
2019-08-01 21:10:46 +0900

    400Error

--- secondproject_assignment/secondproject/urls.py
+++ secondproject_assignment/secondproject/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name = "home"),
-    #path('accounts/',include('allauth.urls')),
+    path('accounts/',include('allauth.urls')),
     
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
